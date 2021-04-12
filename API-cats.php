9d7eed0fe3aba55fb8c96d726bb5833f890f6035
AndrewVakaryuk <andreyvakaryuk@gmail.com>
2021-04-12 13:14:57 +0300

    base architecture for the plagin

---
+++ API-cats.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: API-cats
+Plugin URI: https://github.com/AndrewVakaryuk
+Description: Used by millions(it's a joke) to enjoy cats.
+Version: 1.0.0
+Author: Andrii Vakariuk
+Author URI: https://github.com/AndrewVakaryuk
+License: GPLv2 or later
+Text Domain: API-cats
+*/
+
+defined("ABSPATH") or die();
+
